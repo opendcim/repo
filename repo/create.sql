2edaf757b1f9859f8dd371de066fc5e6864234e1
openDCIM Account <dcim@list.cadmuslabs.net>
2015-02-15 14:16:51 -0500

    Initial commit

---
+++ repo/create.sql
@@ -0,0 +1,14 @@
+--
+-- Database structure for openDCIM Template Repository
+--
+
+DROP TABLE IF EXISTS Users;
+
+DROP TABLE IF EXISTS Manufacturers;
+
+DROP TABLE IF EXISTS DeviceTemplates;
+
+DROP TABLE IF EXISTS CDUTemplates;
+
+DROP TABLE IF EXISTS SensorTemplates;
+
