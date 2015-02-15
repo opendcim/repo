17a4cb5dd77ea69058ccaae0cb533a0a984bb887
openDCIM Account <dcim@list.cadmuslabs.net>
2015-02-15 14:34:29 -0500

    Added the Users and Manufacturers table to get started on this.

--- repo/create.sql
+++ repo/create.sql
@@ -2,9 +2,35 @@
 -- Database structure for openDCIM Template Repository
 --
 
+--
+-- UserID is email address, so no need to keep it as a separate field
+-- 
+
 DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+  UserID VARCHAR(255) NOT NULL,
+  PrettyName VARCHAR(255) NOT NULL,
+  PasswordHash VARCHAR(255) NOT NULL,
+  APIKey VARCHAR(255) NOT NULL,
+  LastLoginAddress VARCHAR(80) DEFAULT NULL,
+  LastLogin DATETIME DEFAULT NULL,
+  LastAPIAddress VARCHAR(80) DEFAULT NULL,
+  LastAPILogin DATETIME DEFAULT NULL,
+  Disabled TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY(UserID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS Manufacturers;
+CREATE TABLE Manufacturers (
+  ManufacturerID INT(11) NOT NULL AUTO_INCREMENT,
+  Name VARCHAR(80) NOT NULL,
+  SubmittedBy VARCHAR(255) NOT NULL,
+  SubmissionDate DATETIME DEFAULT NULL,
+  ApprovedBy VARCHAR(255) NOT NULL,
+  ApprovedDate DATETIME DEFAULT NULL,
+  PRIMARY KEY (ManufacturerID),
+  UNIQUE KEY (Name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS DeviceTemplates;
 
